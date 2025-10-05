8f89586892fe2d0c133c1c3eb295131e3919501b
Nethras2701 <subramanibhagya22@gmail.com>
2025-10-05 11:17:05 +0530

    Update settings.py

--- myproject/myproject/settings.py
+++ myproject/myproject/settings.py
@@ -124,10 +124,12 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/5.2/howto/static-files/
 
-STATIC_URL = 'static/'
+# Static files
+STATIC_URL = '/static/'  # <-- must start and end with a slash
 STATICFILES_DIRS = [
-    BASE_DIR / 'static',
+    os.path.join(BASE_DIR, 'static'),  # dev static files
 ]
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')  # production collected files
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/5.2/ref/settings/#default-auto-field
